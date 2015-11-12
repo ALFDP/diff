fa32f82d951d91835f07758671539e577a03f9be
Sorion <maxime.pelte@gmail.com>
2015-11-12 16:11:21 +0100

    add display function & remove temp realloc

--- src/main.c
+++ src/main.c
@@ -8,26 +8,29 @@ int main(int argc, char** argv)
 	printf("Entry point : \n");
 	printf("1. Checking argc parameter:\n");
 
-	if (argc != 2) {
+	if (argc != 3) {
 		printf("ERROR: invalid argc parameter\n");
 		return 1;
 	}
 	unsigned int i = 0;
-	printf("Valid argc parameter ... \n");
-
 	FileContent* file1;
+	FileContent* file2;
+
+	printf("Valid argc parameter: %d\n", argc);
 	printf("File 1 path: %s\n", argv[1]);
+	printf("File 2 path: %s\n", argv[2]);
+
 	file1 = getFileElem(argv[1]);
-	printf("Struct number of line: %d\n", file1->nbLine);
+	printf("Struct->elem: number of line: %d\n", file1->nbLine);
 
-	
+	file2 = getFileElem(argv[2]);
+	printf("Struct->elem: number of line: %d\n", file2->nbLine);
 
-	for (i = 0; i < file1->nbLine; i++)
-	{
-		printf("%d %s\n", i + 1, file1->elem[i]);
-	}
+	structDisplay(file1, TRUE);
+	structDisplay(file2, TRUE);
 
 	freeStruct(file1);
+	freeStruct(file2);
 
 	system("PAUSE");
 
