34cb298e535b3c9ab55392ef546615cb757992f8
Sorion <maxime.pelte@gmail.com>
2015-11-14 19:36:17 +0100

    Add prototype

--- src/options.h
+++ src/options.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#define TRUE 1
+#define FALSE 0
+
+typedef enum OutputMode OutputMode;
+enum OutputMode
+{
+	NORMAL_OUTPUT_MODE = 0x01,
+	UNIFIED_OUTPUT_MODE = 0x02,
+	SENSITIVE_CASE = 0x04,
+	NO_SENSITIVE_CASE = 0x08
+};
+
+
+void getOptions(int argc, char** argv);
