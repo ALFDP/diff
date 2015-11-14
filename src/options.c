9753cdb16df73c51166e928f2198ffe0b62f443a
Sorion <maxime.pelte@gmail.com>
2015-11-14 18:50:49 +0100

    adding options.c & options.h

---
+++ src/options.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "options.h"
+
+void getNumberOption(int argc, char** argv) {
+	if (argc > 3)
+	{
+		int length = strlen(argv[1]), i = 0;
+
+	}
+	else
+	{
+		// No options
+		// To simple comparaison
+	}
+}
