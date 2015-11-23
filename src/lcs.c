601b93b4595bfdf9f885a3ef157b907ca18b452b
Sorion <maxime.pelte@gmail.com>
2015-11-23 17:52:58 +0100

    Adding lcs

--- src/lcs.c
+++ src/lcs.c
@@ -50,14 +50,15 @@ unsigned int** LCS_buildMatrix(char **leftIn, char **rightIn, unsigned int leftS
 char** LCS_extract(unsigned int** matrix, char** leftIn, char** rightIn, int leftSize, int rightSize, unsigned char isCaseSensitive)
 {
     StringComparator compare = getComparisonMethod(isCaseSensitive);
-    char **lcs = (char**)scalloc((leftSize>rightSize?leftSize:rightSize) * sizeof(char*));
+	int taille = leftSize > rightSize ? leftSize : rightSize;
+    char **lcs = (char**)malloc((leftSize>rightSize?leftSize:rightSize) * sizeof(char*));
 
-    LCS_recursiveExtract(lcs, matrix, leftIn, rightIn, leftSize, rightSize, compare, 0);
+    LCS_recursiveExtract(&lcs, matrix, leftIn, rightIn, leftSize, rightSize, compare, 0);
 
     return lcs;
 }
 
-void LCS_recursiveExtract(char** lcs, unsigned int** matrix, char** leftIn, char** rightIn, int leftSize, int rightSize,
+void LCS_recursiveExtract(char*** lcs, unsigned int** matrix, char** leftIn, char** rightIn, int leftSize, int rightSize,
                           StringComparator compare, unsigned int index)
 {
     if (leftSize == 0 || rightSize == 0)
@@ -66,16 +67,16 @@ void LCS_recursiveExtract(char** lcs, unsigned int** matrix, char** leftIn, char
     if (compare(leftIn[leftSize - 1], rightIn[rightSize - 1]) == 0)
     {
         LCS_recursiveExtract(lcs, matrix, leftIn, rightIn, leftSize - 1, rightSize - 1, compare, index);
-        lcs[index] = leftIn[leftSize - 1];
+        (*lcs)[index] = leftIn[leftSize - 1];
     }
     else if (matrix[leftSize][rightSize] != matrix[leftSize][rightSize - 1])
     {
-        LCS_recursiveExtract(lcs, matrix, leftIn, rightIn, leftSize - 1, rightSize, compare, index);
+        LCS_recursiveExtract(lcs, matrix, leftIn, rightIn, leftSize, rightSize - 1, compare, index);
     }
 
     else
     {
-        LCS_recursiveExtract(lcs, matrix, leftIn, rightIn, leftSize, rightSize - 1, compare, index);
+        LCS_recursiveExtract(lcs, matrix, leftIn, rightIn, leftSize - 1, rightSize, compare, index);
     }
 }
 
@@ -94,10 +95,11 @@ Allocates and initializes a matrix
 void** allocateMatrix(unsigned int xSize, unsigned int ySize, unsigned int elementSize, void* defaultValue)
 {
     void **matrix = NULL;
+	unsigned int *mat = NULL;
     register unsigned int i, j;
 
-    //matrix = smalloc(xSize * sizeof(void*));
-	matrix = malloc(xSize * sizeof(void*));
+    matrix = smalloc(xSize * sizeof(void*));
+
     for (i = 0 ; i < xSize ; i++)
     {
         if(ySize > 0)
@@ -108,8 +110,7 @@ void** allocateMatrix(unsigned int xSize, unsigned int ySize, unsigned int eleme
 
     for (j = 0 ; j < ySize  ; j++)
     {
-		//memcpy(matrix + i + j * elementSize, defaultValue, elementSize);
-		memcpy(matrix[i] + j * elementSize, defaultValue, elementSize);
+		memcpy((void**)(&(matrix[i])) + j * elementSize, defaultValue, elementSize);
     }
 
     return matrix;
