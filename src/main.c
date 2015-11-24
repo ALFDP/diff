49a8a35a61bc59f9090dd06ad52c074404c15729
Sorion <maxime.pelte@gmail.com>
2015-11-24 23:11:30 +0100

    Adding folder or file test

--- src/main.c
+++ src/main.c
@@ -17,49 +17,84 @@ int main(int argc, char** argv)
 		return 1;
 	}
 	unsigned int i = 0;
-	FileContent* file1;
-	FileContent* file2;
+	int folderStatus1 = 0, folderStatus2 = 0;
+
 
 	if (argc == 3)
 	{
 		unsigned int** matrix = NULL;
 		char ** lcs = NULL;
-		file1 = pushFileToStruct(argv[1]);
-		file2 = pushFileToStruct(argv[2]);
+		
+		folderStatus1 = isFolder(argv[1]);
+		folderStatus2 = isFolder(argv[2]);
+
+		if (folderStatus1 == TRUE && folderStatus2 == TRUE)
+		{
+			// TO FOLDER 
+		}
+		else if (folderStatus1 == FALSE && folderStatus2 == FALSE)
+		{
+			FileContent* file1;
+			FileContent* file2;
+
+			file1 = pushFileToStruct(argv[1]);
+			file2 = pushFileToStruct(argv[2]);
 
 
-		structDisplay(file1, TRUE);
-		structDisplay(file2, TRUE);
+			structDisplay(file1, TRUE);
+			structDisplay(file2, TRUE);
 
 
-		printf("%s \n", file1->modifiedTime);
+			printf("%s \n", file1->modifiedTime);
 
-		matrix = LCS_buildMatrix(file1->elem, file2->elem, file1->nbLine, file2->nbLine, TRUE);
-		lcs = LCS_extract(matrix, file1->elem, file2->elem, file1->nbLine, file2->nbLine, TRUE);
-		printNormalDiff(file1->elem, file2->elem, lcs, file1->nbLine, file2->nbLine, TRUE);
+			matrix = LCS_buildMatrix(file1->elem, file2->elem, file1->nbLine, file2->nbLine, TRUE);
+			lcs = LCS_extract(matrix, file1->elem, file2->elem, file1->nbLine, file2->nbLine, TRUE);
+			printNormalDiff(file1->elem, file2->elem, lcs, file1->nbLine, file2->nbLine, TRUE);
 
-		freeStruct(file1);
-		freeStruct(file2);
+			freeStruct(file1);
+			freeStruct(file2);
+		}
 	}
 
 	else if (argc == 4)
 	{
-		OutputMode option;
-		option = getOptions(0, argv);
+		unsigned int** matrix = NULL;
+		char ** lcs = NULL;
 
-		printf("option %d\n", option);
-	
-		file1 = pushFileToStruct(argv[2]);
-		file2 = pushFileToStruct(argv[3]);
+		folderStatus1 = isFolder(argv[1]);
+		folderStatus2 = isFolder(argv[2]);
 
-		structDisplay(file1, TRUE);
-		structDisplay(file2, TRUE);
+		if (folderStatus1 == TRUE && folderStatus2 == TRUE)
+		{
+			// TO FOLDER 
+		}
+		else if (folderStatus1 == FALSE && folderStatus2 == FALSE)
+		{
+			FileContent* file1;
+			FileContent* file2;
+			OutputMode option;
 
-		freeStruct(file1);
-		freeStruct(file2);
+			option = getOptions(0, argv);
+
+			printf("option %d\n", option);
+
+			file1 = pushFileToStruct(argv[1]);
+			file2 = pushFileToStruct(argv[2]);
+
+
+			structDisplay(file1, TRUE);
+			structDisplay(file2, TRUE);
+
+
+			printf("%s \n", file1->modifiedTime);
+
+			matrix = LCS_buildMatrix(file1->elem, file2->elem, file1->nbLine, file2->nbLine, TRUE);
+			lcs = LCS_extract(matrix, file1->elem, file2->elem, file1->nbLine, file2->nbLine, TRUE);
+			printNormalDiff(file1->elem, file2->elem, lcs, file1->nbLine, file2->nbLine, TRUE);
+
+			freeStruct(file1);
+			freeStruct(file2);
+		}
 	} 
-	
-	getFolder(argv[1]);
-
 	return 0;
 }
