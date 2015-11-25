38c97ab96f094697a5759654f5ce6473f6b6356d
Sorion <maxime.pelte@gmail.com>
2015-11-26 00:52:47 +0100

    Merge

--- src/main.c
+++ src/main.c
@@ -16,112 +16,136 @@ int main(int argc, char** argv)
 		printf("ERROR: invalid argc parameter\n");
 		return 1;
 	}
-	unsigned int i = 0;
-	int folderStatus1 = 0, folderStatus2 = 0;
 
+	int folderStatus1 = 0, folderStatus2 = 0;
 
 	if (argc == 3)
 	{
-		unsigned int** matrix = NULL;
-		char ** lcs = NULL;
-		
-		
 		folderStatus1 = isFolder(argv[1]);
 		folderStatus2 = isFolder(argv[2]);
 
 		if (folderStatus1 == TRUE && folderStatus2 == TRUE)
 		{
-			// TO FOLDER 
-		}
-		else if (folderStatus1 == FALSE && folderStatus2 == FALSE)
-		{
-			FileContent* file1;
-			FileContent* file2;
-			unsigned int lcsSize = 0;
+			char** fileName1 = NULL;
+			char** fileName2 = NULL;
+			int fileNameLen1 = 0, fileNameLen2 = 0, i = 0;
 
-			file1 = pushFileToStruct(argv[1]);
-			file2 = pushFileToStruct(argv[2]);
+			fileName1 = getFolder(argv[1], &fileNameLen1);
+			fileName2 = getFolder(argv[2], &fileNameLen2);
+			compareFolderFile(argv, 0, argv[1], argv[2], fileName1, fileName2, fileNameLen1, fileNameLen2);
 
-			matrix = LCS_buildMatrix(file1->elem, file2->elem, file1->nbLine, file2->nbLine, TRUE);
-			lcs = LCS_extract(&lcsSize, matrix, &file1->elem, &file2->elem, file1->nbLine, file2->nbLine, TRUE);
-			printStandardDiff(file1->elem, file2->elem, matrix, lcsSize, file1->nbLine, file2->nbLine, TRUE);
-			
-			freeStruct(file1);
-			freeStruct(file2);
-		}
-	}
-
-	else if (argc == 4)
-	{
-		unsigned int** matrix = NULL;
-		char ** lcs = NULL;
-
-		folderStatus1 = isFolder(argv[1]);
-		folderStatus2 = isFolder(argv[2]);
-
-		if (folderStatus1 == TRUE && folderStatus2 == TRUE)
-		{
-			// TO FOLDER 
+			for (i = 0; i < fileNameLen1; i++)
+				free(fileName1[i]);
+			free(fileName1);
+			for (i = 0; i < fileNameLen2; i++)
+				free(fileName2[i]);
+			free(fileName2);
 		}
 		else if (folderStatus1 == FALSE && folderStatus2 == FALSE)
 		{
 			FileContent* file1;
 			FileContent* file2;
 			OutputMode option;
-			unsigned int lcsSize = 0;
-			int res;
 
+			file1 = pushFileToStruct(argv[1]);
+			file2 = pushFileToStruct(argv[2]);
+
+			option = NORMAL_OUTPUT_MODE + SENSITIVE_CASE;
+
+			redirectOutput(option, argv[1], argv[2], file1, file2);
+			
+			freeStruct(file1);
+			freeStruct(file2);
+		}
+		else
+		{
+			char** filesName = NULL;
+			int filesNameLen = 0, i = 0;
+
+			if (folderStatus1 == TRUE)
+			{
+				filesName = getFolder(argv[1], &filesNameLen);
+				compareFileAndFolder(argv, 0, argv[1], filesName, filesNameLen, argv[2]);
+				for (i = 0; i < filesNameLen; i++)
+					free(filesName[i]);
+				free(filesName);
+
+			}
+			else if (folderStatus2 == TRUE)
+			{
+				filesName = getFolder(argv[2], &filesNameLen);
+				compareFileAndFolder(argv, 0, argv[2], filesName, filesNameLen, argv[1]);
+				for (i = 0; i < filesNameLen; i++)
+					free(filesName[i]);
+				free(filesName);
+			}
+		}
+	}
+
+	else if (argc == 4)
+	{
+
+
+		folderStatus1 = isFolder(argv[2]);
+		folderStatus2 = isFolder(argv[3]);
+
+		if (folderStatus1 == TRUE && folderStatus2 == TRUE)
+		{
+			char** fileName1 = NULL;
+			char** fileName2 = NULL;
+			int fileNameLen1 = 0, fileNameLen2 = 0, i = 0;
+
+			fileName1 = getFolder(argv[2], &fileNameLen1);
+			fileName2 = getFolder(argv[3], &fileNameLen2);
+			compareFolderFile(argv, 1, argv[2], argv[3], fileName1, fileName2, fileNameLen1, fileNameLen2);
+
+			for (i = 0; i < fileNameLen1; i++)
+				free(fileName1[i]);
+			free(fileName1);
+			for (i = 0; i < fileNameLen2; i++)
+				free(fileName2[i]);
+			free(fileName2);
+		}
+		else if (folderStatus1 == FALSE && folderStatus2 == FALSE)
+		{
+			FileContent* file1;
+			FileContent* file2;
+			OutputMode option;
+			
 			option = getOptions(0, argv);
 
 			file1 = pushFileToStruct(argv[2]);
 			file2 = pushFileToStruct(argv[3]);
 
-			matrix = LCS_buildMatrix(file1->elem, file2->elem, file1->nbLine, file2->nbLine, TRUE);
-			lcs = LCS_extract(&lcsSize, matrix, &file1->elem, &file2->elem, file1->nbLine, file2->nbLine, TRUE);
-
-			if (option == NORMAL_OUTPUT_MODE + SENSITIVE_CASE)
-			{
-				matrix = LCS_buildMatrix(file1->elem, file2->elem, file1->nbLine, file2->nbLine, TRUE);
-				lcs = LCS_extract(&lcsSize, matrix, &file1->elem, &file2->elem, file1->nbLine, file2->nbLine, TRUE);
-				printStandardDiff(file1->elem, file2->elem, matrix, lcsSize, file1->nbLine, file2->nbLine, TRUE);
-			}
-			else if (option == UNIFIED_OUTPUT_MODE + SENSITIVE_CASE)
-			{
-				matrix = LCS_buildMatrix(file1->elem, file2->elem, file1->nbLine, file2->nbLine, TRUE);
-				lcs = LCS_extract(&lcsSize, matrix, &file1->elem, &file2->elem, file1->nbLine, file2->nbLine, TRUE);
-				printUnifiedDiff(argv[2], argv[3], file1->elem, file1->nbLine, file2->elem, file2->nbLine, matrix, lcsSize, TRUE, 3);
-			}
-
-			else if (option == NORMAL_OUTPUT_MODE + NO_SENSITIVE_CASE)
-			{
-				matrix = LCS_buildMatrix(file1->elem, file2->elem, file1->nbLine, file2->nbLine, FALSE);
-				lcs = LCS_extract(&lcsSize, matrix, &file1->elem, &file2->elem, file1->nbLine, file2->nbLine, FALSE);
-				printStandardDiff(file1->elem, file2->elem, matrix, lcsSize, file1->nbLine, file2->nbLine, FALSE);
-			}
-			else if (option == UNIFIED_OUTPUT_MODE + NO_SENSITIVE_CASE)
-			{
-				matrix = LCS_buildMatrix(file1->elem, file2->elem, file1->nbLine, file2->nbLine, FALSE);
-				lcs = LCS_extract(&lcsSize, matrix, &file1->elem, &file2->elem, file1->nbLine, file2->nbLine, FALSE);
-				printUnifiedDiff(argv[2], argv[3], file1->elem, file1->nbLine, file2->elem, file2->nbLine, matrix, lcsSize, FALSE, 3);
-			}
-
-			else if (option == BRIEF_REPORT + SENSITIVE_CASE + NORMAL_OUTPUT_MODE)
-				res = printReport(file1, file2, 0, TRUE);
-
-			else if (option == IDENTICAL_REPORT + SENSITIVE_CASE + NORMAL_OUTPUT_MODE)
-				res = printReport(file1, file2, 1, TRUE);
-
-			else if (option == BRIEF_REPORT + NO_SENSITIVE_CASE + NORMAL_OUTPUT_MODE)
-				res = printReport(file1, file2, 0, FALSE);
-
-			else if (option == IDENTICAL_REPORT + NO_SENSITIVE_CASE + NORMAL_OUTPUT_MODE)
-				res = printReport(file1, file2, 1, FALSE);
-
-
+			redirectOutput(option, argv[2], argv[3], file1, file2);
 
 			freeStruct(file1);
 			freeStruct(file2);
 		}
+		else
+		{
+			char** filesName = NULL;
+			int filesNameLen = 0, i = 0;
+
+			if (folderStatus1 == TRUE)
+			{
+				filesName = getFolder(argv[2], &filesNameLen);
+				compareFileAndFolder(argv, 0, argv[2], filesName, filesNameLen, argv[3]);
+				for (i = 0; i < filesNameLen; i++)
+					free(filesName[i]);
+				free(filesName);
+
+			}
+			else if (folderStatus2 == TRUE)
+			{
+				filesName = getFolder(argv[3], &filesNameLen);
+				compareFileAndFolder(argv, 0, argv[3], filesName, filesNameLen, argv[2]);
+				for (i = 0; i < filesNameLen; i++)
+					free(filesName[i]);
+				free(filesName);
+			}
+		}
+
 	} 
 	return 0;
 }
