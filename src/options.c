2f9e05067e9e13354abb6ba34801862081992bff
Sorion <maxime.pelte@gmail.com>
2015-11-26 00:49:11 +0100

    Improving option getter

--- src/options.c
+++ src/options.c
@@ -30,7 +30,6 @@ OutputMode getOptions(int argc, char** argv) {
 				getLongOption(argv, &i, &options, tabOption, FALSE);
 			}
 		}
-
 	}
 
 	if (tabOption[0] == FALSE)
@@ -50,51 +49,52 @@ int getLongOption(char** argv, int* val, OutputMode* option,int* checkTab, int l
 
 	if (longChar == FALSE)
 	{
+		while ((argv[1][i] != '-') ||( argv[1][i] != '\0'))
+		{
+			if (argv[1][i] == '\0')
+				return 0;
+
 			tab[j] = argv[1][i];
+			*val = i++;
 
+			if (tab[j] == 'u')
+			{
+				if (checkTab[0] == FALSE)
+				{
+					*option = *option + UNIFIED_OUTPUT_MODE;
+					checkTab[0] = TRUE;
+				}
+			}
+			else if (tab[j] == 'i')
+			{
+				if (checkTab[1] == FALSE)
+				{
+					*option = *option + SENSITIVE_CASE;
+					checkTab[1] = TRUE;
+					
+				}
+			}
+			else if (tab[j] == 'q')
+			{
+				if (checkTab[2] == FALSE)
+				{
+					*option = *option + BRIEF_REPORT;
+					checkTab[2] = TRUE;
+					
+				}
+				
+			}
+			else if (tab[j] == 's')
+			{
+				if (checkTab[2] == FALSE && checkTab[3] == FALSE)
+				{
+					*option = *option + IDENTICAL_REPORT;
+					checkTab[3] = TRUE;	
+				}
+			}
 			j++;
-
-		*val = i;
-
-		if (tab[0] == 'u')
-		{
-			if (checkTab[0] == FALSE)
-			{
-				*option = *option + UNIFIED_OUTPUT_MODE;
-				checkTab[0] = TRUE;
-			}
 		}
-		if (tab[0] == 'i')
-		{
-			if (checkTab[1] == FALSE)
-			{
-				*option = *option + SENSITIVE_CASE;
-				checkTab[1] = TRUE;
-				return TRUE;
-			}
-			return FALSE;
-		}
-		if (tab[0] == 'q')
-		{
-			if (checkTab[2] == FALSE)
-			{
-				*option = *option + BRIEF_REPORT;
-				checkTab[2] = TRUE;
-				return TRUE;
-			}
-			return FALSE;
-		}
-		if (tab[0] == 's')
-		{
-			if (checkTab[2] == FALSE && checkTab[3] == FALSE)
-			{
-				*option = *option + IDENTICAL_REPORT;
-				checkTab[3] = TRUE;
-				return TRUE;
-			}
-			return FALSE;
-		}
-
+		return 0;
 	}
 
 	else
@@ -160,7 +160,7 @@ void initTab(int* tab, int size)
 	}
 }
 
-int printReport(FileContent* file1, FileContent* file2, int option, int caseSensitive)
+int printReport(FileContent* file1, FileContent* file2, int option, OutputMode options, int caseSensitive)
 {
 	int i = 0, j = 0, lineF1 = file1->nbLine, lineF2 = file2->nbLine;
 	char** txt1 = file1->elem;
@@ -172,6 +172,11 @@ int printReport(FileContent* file1, FileContent* file2, int option, int caseSens
 	{
 		if (option == 0)
 			printf("Files are different\n");
+		else
+		{
+			options = options - IDENTICAL_REPORT;
+			redirectOutput(options, file1->path, file2->path, file1, file2);
+		}
 
 		return 0;
 	}
@@ -184,6 +189,11 @@ int printReport(FileContent* file1, FileContent* file2, int option, int caseSens
 			{
 				if (option == 0)
 					printf("File are different\n");
+				else
+				{
+					options = options - IDENTICAL_REPORT;
+					redirectOutput(options, file1->path, file2->path, file1, file2);
+				}
 				return 0;
 			}
 		}
@@ -195,3 +205,56 @@ int printReport(FileContent* file1, FileContent* file2, int option, int caseSens
 }
 
 
+void redirectOutput(OutputMode option, char* path1, char* path2, FileContent* file1, FileContent* file2)
+{
+	unsigned int** matrix = NULL;
+	char ** lcs = NULL;
+	unsigned int lcsSize = 0, i = 0;
+
+	if (option == NORMAL_OUTPUT_MODE + SENSITIVE_CASE)
+	{
+		matrix = LCS_buildMatrix(file1->elem, file2->elem, file1->nbLine, file2->nbLine, TRUE);
+		lcs = LCS_extract(&lcsSize, matrix, &file1->elem, &file2->elem, file1->nbLine, file2->nbLine, TRUE);
+		printStandardDiff(file1->elem, file2->elem, matrix, lcsSize, file1->nbLine, file2->nbLine, TRUE);
+		freeMatrix(&matrix, file1->nbLine + 1);
+		free(lcs);
+
+	}
+	else if (option == UNIFIED_OUTPUT_MODE + SENSITIVE_CASE)
+	{
+		matrix = LCS_buildMatrix(file1->elem, file2->elem, file1->nbLine, file2->nbLine, TRUE);
+		lcs = LCS_extract(&lcsSize, matrix, &file1->elem, &file2->elem, file1->nbLine, file2->nbLine, TRUE);
+		printUnifiedDiff(path1, path2, file1->elem, file1->nbLine, file2->elem, file2->nbLine, matrix, lcsSize, TRUE, 3);
+		freeMatrix(&matrix, file1->nbLine +1);
+		free(lcs);
+	}
+
+	else if (option == NORMAL_OUTPUT_MODE + NO_SENSITIVE_CASE)
+	{
+		matrix = LCS_buildMatrix(file1->elem, file2->elem, file1->nbLine, file2->nbLine, FALSE);
+		lcs = LCS_extract(&lcsSize, matrix, &file1->elem, &file2->elem, file1->nbLine, file2->nbLine, FALSE);
+		printStandardDiff(file1->elem, file2->elem, matrix, lcsSize, file1->nbLine, file2->nbLine, FALSE);
+		freeMatrix(&matrix, file1->nbLine + 1);
+		free(lcs);
+	}
+	else if (option == UNIFIED_OUTPUT_MODE + NO_SENSITIVE_CASE)
+	{
+		matrix = LCS_buildMatrix(file1->elem, file2->elem, file1->nbLine, file2->nbLine, FALSE);
+		lcs = LCS_extract(&lcsSize, matrix, &file1->elem, &file2->elem, file1->nbLine, file2->nbLine, FALSE);
+		printUnifiedDiff(path1, path2, file1->elem, file1->nbLine, file2->elem, file2->nbLine, matrix, lcsSize, FALSE, 3);
+		freeMatrix(&matrix, file1->nbLine + 1);
+		free(lcs);
+	}
+
+	else if ((option == BRIEF_REPORT + SENSITIVE_CASE + NORMAL_OUTPUT_MODE) || (option == BRIEF_REPORT + SENSITIVE_CASE + UNIFIED_OUTPUT_MODE))
+		printReport(file1, file2, 0, option, TRUE);
+
+	else if (option == IDENTICAL_REPORT + SENSITIVE_CASE + NORMAL_OUTPUT_MODE || option == IDENTICAL_REPORT + SENSITIVE_CASE + UNIFIED_OUTPUT_MODE)
+		printReport(file1, file2, 1, option, TRUE);
+
+	else if (option == BRIEF_REPORT + NO_SENSITIVE_CASE + NORMAL_OUTPUT_MODE || option == BRIEF_REPORT + NO_SENSITIVE_CASE + UNIFIED_OUTPUT_MODE)
+		printReport(file1, file2, 0, option, FALSE);
+
+	else if (option == IDENTICAL_REPORT + NO_SENSITIVE_CASE + NORMAL_OUTPUT_MODE || option == IDENTICAL_REPORT + NO_SENSITIVE_CASE + UNIFIED_OUTPUT_MODE)
+		printReport(file1, file2, 1, option, FALSE);
+}
